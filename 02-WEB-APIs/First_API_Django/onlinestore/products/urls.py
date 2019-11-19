85c9dd571ccb2bb51a3ed40d11df90e5e43bd37b
jenhaoyang <randy19962@gmail.com>
2019-11-19 11:29:39 +0800

    add two api endpoint by django

---
+++ 02-WEB-APIs/First_API_Django/onlinestore/products/urls.py
@@ -0,0 +1,7 @@
+from django.urls import path
+from .views import product_detail, product_list
+
+urlpatterns = [
+    path("products", product_list, name="product-list"),
+    path("products/<int:pk>", product_detail, name="product-detail"),
+]
